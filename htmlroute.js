bfe8050577920c5c2fb4381bb0a9eccff20cd1bc
Garrett <gjblythe@gmail.com>
2018-12-13 08:59:17 -0700

    scrap redirect

--- htmlroute.js
+++ htmlroute.js
@@ -5,10 +5,15 @@ module.exports = function(app) {
   app.get("/", function(req, res) {
     db.Article.find({}).then(function(dbArticles) {
       res.render("index", {
-        msg: "Welcome!",
+        msg: "Javascript Libraries",
         articles: dbArticles
     });
     console.log(dbArticles);
     });
   });
+  app.get("/scrape", function(req,res){
+    res.render("scrape", {
+      msg: "Scrape is now complete."
+    })
+  })
 };
